4223430d9168223029c7639149025c79e69b4f37
ahmad <ahmad@dw-10.eecs.uci.edu>
2018-12-04 16:16:31 -0800

    fixed adding file problem

---
+++ gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/src/dhp.cpp" "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/src/dhp.cpp.o"
+  "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/src/dllmain.cpp" "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/src/dllmain.cpp.o"
+  "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/src/hp.cpp" "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/src/hp.cpp.o"
+  "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/src/init.cpp" "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/src/init.cpp.o"
+  "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/src/thread_data.cpp" "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/src/thread_data.cpp.o"
+  "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/src/topology_hpux.cpp" "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/src/topology_hpux.cpp.o"
+  "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/src/topology_linux.cpp" "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/src/topology_linux.cpp.o"
+  "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/src/topology_osx.cpp" "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/src/topology_osx.cpp.o"
+  "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/src/urcu_gp.cpp" "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/src/urcu_gp.cpp.o"
+  "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/src/urcu_sh.cpp" "/home/ahmad/Downloads/benchmark-fuzzying-tool/gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds-s.dir/src/urcu_sh.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ gdax-orderbook-hpp/demo/dependencies/libcds-2.3.2/build-release/CMakeFiles/cds.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cds.dir/src/init.cpp.o"
+  "CMakeFiles/cds.dir/src/hp.cpp.o"
+  "CMakeFiles/cds.dir/src/dhp.cpp.o"
+  "CMakeFiles/cds.dir/src/urcu_gp.cpp.o"
+  "CMakeFiles/cds.dir/src/urcu_sh.cpp.o"
+  "CMakeFiles/cds.dir/src/thread_data.cpp.o"
+  "CMakeFiles/cds.dir/src/topology_hpux.cpp.o"
+  "CMakeFiles/cds.dir/src/topology_linux.cpp.o"
+  "CMakeFiles/cds.dir/src/topology_osx.cpp.o"
+  "CMakeFiles/cds.dir/src/dllmain.cpp.o"
+  "bin/libcds.pdb"
+  "bin/libcds.so"
+  "bin/libcds.so.2.3.2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
